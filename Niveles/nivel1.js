8bde073b753baf0ea247f2f6eab2ea198caf308c
juancif <cifuenteslozanojuandavid@gmail.com>
2024-08-14 11:14:05 -0500

    Se generan dashboards de ganar o perder ademas de su carpeta, se genera codigo js para funciones de movimiento basicas y se consigue la redireccion al cumplir con ciertas condiciones

--- Niveles/nivel1.js
+++ Niveles/nivel1.js
@@ -1,4 +1,34 @@
-primeraPregunta = prompt("Es un elemnto esencial para para la proteccion de los ojos");
+document.addEventListener('DOMContentLoaded', (event) => {
+    const mario = document.getElementById('mario');
+    const resetPosition = document.getElementById('flechaIzquierda');
+    const flechaDerecha = document.getElementById('flechaDerecha');
+    const botonGolpe = document.getElementById('botonGolpe');
+    const yoshi = document.getElementById('yoshi'); // Asegúrate de definir 'yoshi'
+
+    // Funciones para el movimiento
+    flechaIzquierda.addEventListener('click', () => {
+        mario.classList.add('move-left');
+    });
+
+    flechaDerecha.addEventListener('click', () => {
+        mario.classList.add('move-right');
+    });
+
+    botonGolpe.addEventListener('click', () => {
+        const input = prompt("¿Elemento de seguridad para los ojos?");
+
+        if (input === "gafas") {
+            yoshi.classList.add('transparent', 'translate-up');
+
+            window.location.href = '/Niveles/Segundo_nivel.html'
+
+        } else {
+            window.location.href = '/Index/index.html';
+        }
+    });
 
 
-if (primeraPregunta === "gafas");
+    resetPosition.addEventListener('click', () => {
+        mario.classList.remove('move-left', 'move-right', 'scale-up');
+    });
+});
